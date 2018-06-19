e1d8bdb158fd8548d55624a82b2aa06f9b405b3e
yertle75 <jaioublielemotdepasse@tuta.io>
2018-06-19 15:37:25 +0100

    added i3 configs

--- copy.sh
+++ copy.sh
@@ -1,3 +1,7 @@
 echo -n "copying vimrc ..."
 cp ~/.vimrc vimrc
 echo "done"
+
+echo -n "copying i3config ..."
+cp ~/.config/i3/config i3config
+echo "done"
--- paste.sh
+++ paste.sh
@@ -2,3 +2,7 @@
 echo -n "pasting vimrc..."
 cp vimrc ~/.vimrc 
 echo "done"
+
+echo -n "pasting i3config..."
+cp i3config ~/.config/i3/config
+echo "done"
