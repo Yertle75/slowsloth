154b74c8d2d362d80407d8228bb81e14a6103915
Julien Du Crest <v1jducr@godfrey.inf.ed.ac.uk>
2018-06-25 11:53:25 +0100

    fixed no i3 dir error

--- copy.sh
+++ copy.sh
@@ -2,6 +2,10 @@ echo -n "copying vimrc ..."
 cp ~/.vimrc vimrc
 echo "done"
 
-echo -n "copying i3config ..."
-cp ~/.config/i3/config i3config
-echo "done"
+echo -n "trying to copy i3config ..."
+if [ -d ~/.config/i3 ] then
+	cp ~/.config/i3/config i3config
+	echo "done"
+else
+	echo "i3 directory doesn't exists... FAIL"
+fi
--- paste.sh
+++ paste.sh
@@ -3,6 +3,10 @@ echo -n "pasting vimrc..."
 cp vimrc ~/.vimrc 
 echo "done"
 
-echo -n "pasting i3config..."
+echo -n "trying to paste i3config..."
+if [ -d ~/.config/i3 ]; then
 cp i3config ~/.config/i3/config
 echo "done"
+else 
+	echo "i3 directory doesn't exists... FAIL"
+fi
