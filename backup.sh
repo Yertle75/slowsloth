8bb2effcd74db2afc4492b3d3df631297dbd276e
Yertle75 <jducrest@posteo.net>
2019-09-25 10:37:15 +0200

    changed bash and new i3 backup

--- backup.sh
+++ backup.sh
@@ -3,7 +3,8 @@ cp ~/.vimrc vimrc
 echo "done"
 
 echo -n "trying to copy i3config ..."
-if [ -d ~/.config/i3 ] then
+if [ -d ~/.config/i3 ] 
+then
 	cp ~/.config/i3/config i3config
 	echo "done"
 else
